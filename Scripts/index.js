0a1bbcfd97ad27c2d8ab6b83d6c667711984eedf
Manoj Kumar G <manoj.kumar@moonraft.com>
2023-08-24 19:51:47 +0530

    Close Warning Functionality

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,10 @@
-function dropDown() {
-    document.getElementById("dropDown").classList.toggle("show");
-    console.log("show");
-  }
+function dropDown() 
+{
+  document.getElementById("dropDown").classList.toggle("show");
+  console.log("show");
+}
+function closeWarning()
+{
+  document.getElementById("warning").remove();
+  document.getElementById("info").style.marginTop ="30px";
+}
