f276fe0d1b1318961717ee1c54ce75eb55216245
Manoj Kumar G <manoj.kumar@moonraft.com>
2023-08-25 19:02:40 +0530

    Dynamic Carousel

--- Scripts/index.js
+++ Scripts/index.js
@@ -7,7 +7,8 @@ const jsonObject = `{
     "NumberofShares" : "50",
     "Type" : "VBS Security",
     "Amount" : "$30,000.00",
-    "Status" : "Entered"},
+    "Status" : "Entered"
+  },
   "Contributions" : {
       "info" : "Vanguard New Jersey Long-Term Tax-Exempt Fund Admiral Shares of Contributions.",
       "EnteredDate" : "10/07/1987",
@@ -34,7 +35,29 @@ const jsonObject = `{
       "Type" : "VBS Security",
       "Amount" : "$100,000.00",
       "Status" : "Exit"
-  }
+  },
+  "Carousel": [
+    {
+        "Title" : "Featured-1",
+        "column1Content" : "Learn what are grants",
+        "column2Content" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae praesentium, doloremque quaerat aliquid eveniet, aperiam et rem, beatae minima necessitatibus delectus adipisci debitis sed cum magni voluptatum quisquam nesciunt qui libero veniam pariatur iste? Voluptatibus, nisi?"
+    },
+    {
+        "Title" : "Featured-2",
+        "column1Content" : "Learn more about Conribution",
+        "column2Content" : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta ut maiores cum maxime rem error eligendi facere. Modi, provident quae."
+    },
+    {
+        "Title" : "Featured-3",
+        "column1Content" : "Learn how exchange works and backend process of it",
+        "column2Content" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eligendi dolorum, quae rem culpa commodi explicabo quisquam quis beatae vitae perferendis maiores voluptates? Sequi quas ullam dolore saepe! Odit illum ullam quos exercitationem ut nam doloremque omnis dolores ratione esse, vel at natus a cum!"
+    },
+    {
+        "Title" : "Featured-4",
+        "column1Content" : "Learn the concept of Shares",
+        "column2Content" : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, rem sapiente soluta accusamus placeat atque velit temporibus, voluptas, porro doloribus cum vitae vero. Beatae explicabo nulla quas quo facere ad repudiandae necessitatibus ducimus illo, impedit, hic quod libero. Fugit atque at provident pariatur esse dolores accusamus veritatis asperiores a impedit."
+    }
+  ]
 }`
 var data = JSON.parse(jsonObject);
 function dropDown() 
@@ -92,4 +115,27 @@ function updateData()
   document.getElementById("shares").innerHTML=selectedOption.NumberofShares;
   document.getElementById("amount").innerHTML=selectedOption.Amount;
   document.getElementById("status").innerHTML=selectedOption.Status;
+}
+var pageNumberIndex=0;
+var carouselContent=data.Carousel[pageNumberIndex];
+function fillCarousel()
+{
+  document.getElementById("carouselTitle").innerHTML=carouselContent.Title;
+  document.getElementById("carouselContent1").innerHTML=carouselContent.column1Content;
+  document.getElementById("carouselContent2").innerHTML=carouselContent.column2Content;
+}
+function nextPage()
+{
+    pageNumberIndex++;
+    pageNumberIndex=pageNumberIndex%4;
+    carouselContent=data.Carousel[pageNumberIndex];
+    fillCarousel();
+}
+function previousPage()
+{
+  pageNumberIndex--;
+  if(pageNumberIndex<0)
+     pageNumberIndex=3;
+  carouselContent=data.Carousel[pageNumberIndex];
+  fillCarousel();
 }
